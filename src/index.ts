851b079ac55048cc6aeb1f8b5f0bea4c99b7c78d
Leandro Rocha <prog.leandro@gmail.com>
2021-03-28 12:34:26 -0300

    Finalizando a documentacao

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 import { usersRoutes } from "./routes/users.routes";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument) )
 
 app.use("/users", usersRoutes);
 
